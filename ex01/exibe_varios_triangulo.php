fb10ea8958d0b8a6471de83ad3002b312ba5fcc2
Fopor <php.22@batatinha.cec.ime.usp.br>
2019-01-29 22:05:52 -0200

    added program that print many tirangles

---
+++ ex01/exibe_varios_triangulo.php
@@ -0,0 +1,19 @@
+<?php
+
+function makeTri($size) {
+	$size_aux = 0;
+
+	while($size_aux !== $size){
+		for($i=0;$i<$size_aux;$i++){
+			echo "*";
+		}
+		echo PHP_EOL;
+		$size_aux++;
+	}
+}
+
+makeTri(40);
+makeTri(10);
+makeTri(4);
+?>
+
