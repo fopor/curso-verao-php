e3cf63220f883288c3351c2ae10e4654361d7148
root <root@DESKTOP-K30K7B6.localdomain>
2019-02-03 16:48:30 +0000

    added login page

--- tarefa1-formulario-sql/login.php
+++ tarefa1-formulario-sql/login.php
@@ -21,7 +21,39 @@
   </ul>
 </nav>
 
-<h1>Login do usuário</h1>
+<div class="container">
+    <h1>Login do usuário</h1>
+    <form class="form-horizontal" method="post">
+    <div class="form-group">
+        <label class="control-label col-sm-2" for="email">Email:</label>
+        <div class="col-sm-10">
+            <input value="<?php echo $email; ?>"
+                type="email" class="form-control" id="email" placeholder="Coloque o email" name="email"
+                oninvalid="if (this.value == ''){
+                    this.setCustomValidity('Este campo é obrigatório')}
+                    if (this.value != ''){
+                        this.setCustomValidity('Insira um email válido!')}"
+                oninput="setCustomValidity('')"
+            >
+        </div>
+        </div>
+
+        <div class="form-group">
+        <label class="control-label col-sm-2" for="pwd">Senha:</label>
+        <div class="col-sm-10">          
+            <input type="password" class="form-control" id="pwd" placeholder="Insira a senha" name="pass"
+            value="<?php if($pass_set) echo "$pass";?>"
+            >
+        </div>
+        </div>
+
+        <div class="form-group">        
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Entrar</button>
+        </div>
+        </div>
+    </form>
+</div>
 
 </body>
 </html>
